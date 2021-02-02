c2abdf1e6b09643db1677530ba211ca404442984
Iago Caran Aquino <iago.caran@gmail.com>
2021-02-02 01:24:30 +0100

    Add ts-loader, html-webpack-plugin and clean-webpack-plugin and configure them in the project

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.ts',
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Babylon project',
+            template: 'src/index.html'
+        }),
+        new CleanWebpackPlugin(['dist']),
+    ],
     output: {
-        filename: 'index.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: [ '.ts', '.js' ],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            }
+        ]
     }
-}
+};
