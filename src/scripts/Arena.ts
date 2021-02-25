ba30162193fba7db07bfdfd81a023baedbfb50d4
Iago Caran Aquino <iago.caran@gmail.com>
2021-02-26 00:41:22 +0100

    Add tile texture and expand Arena

--- src/scripts/Arena.ts
+++ src/scripts/Arena.ts
@@ -9,8 +9,7 @@ import {
     Vector3
 } from "@babylonjs/core";
 import { Game } from "./Game";
-import Brick from '@/images/brick.jpg';
-import Wood from '@/images/wood.jpg';
+import Tile from '@/images/tile.jpg';
 
 export class Arena {
     game: Game;
@@ -19,62 +18,44 @@ export class Arena {
         this.game = game;
         let scene = game.scene;
 
-        let light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
-        light.diffuse = new Color3(1, 1, 1);
-        light.specular = new Color3(1, 1, 1);
-        /*
-        let light1 = new DirectionalLight("dir1", new Vector3(0, -1, 0), scene);
-        light1.diffuse = new Color3(1, 1, 1);
-        light1.specular = new Color3(1, 1, 1);
-         */
-        /*
-        let light2 = new PointLight("omni1", new Vector3(1, 10, 1), scene);
-        light2.diffuse = new Color3(1, 1, 1);
-        light2.specular = new Color3(1, 1, 1);
-         */
-        /*
-        let light3 = new SpotLight("spot1", new Vector3(1, 10, 1), new Vector3(0, -1, 0), 0.8, 2, scene);
-        light3.diffuse = new Color3(1, 1, 1);
-        light3.specular = new Color3(1, 1, 1);
-         */
+        let light = new HemisphericLight("light1", new Vector3(0, 10, 0), scene);
+        let light2 = new HemisphericLight("light2", new Vector3(0, -1, 0), scene);
+        light2.intensity = 0.8;
 
         let materialGround = new StandardMaterial("groundTexture", scene);
-        let brickTexture = new Texture(Brick, scene);
-        brickTexture.uScale = 4.0;
-        brickTexture.vScale = 4.0;
-        materialGround.diffuseTexture = brickTexture;
+        let tileTexture = new Texture(Tile, scene);
+        tileTexture.uScale = 8.0;
+        tileTexture.vScale = 8.0;
+        materialGround.diffuseTexture = tileTexture;
 
         let materialWall = new StandardMaterial("wallTexture", scene);
-        materialWall.diffuseTexture = new Texture(Wood, scene);
+        materialWall.diffuseTexture = new Texture(Tile, scene);
 
-        let ground = Mesh.CreateGround("ground1", 20, 20, 2, scene);
-        ground.scaling = new Vector3(2, 10, 3);
-        ground.scaling.z = 2;
-        ground.material = materialGround;
+        let boxArena = Mesh.CreateBox("box1", 100, scene, false, Mesh.BACKSIDE);
+        boxArena.material = materialGround;
+        boxArena.position.y = 50 * 0.3;
+        boxArena.scaling = new Vector3(3.5, 0.3, 0.8);
 
-        let mainBox = Mesh.CreateBox("box1", 3, scene);
-        mainBox.scaling.y = 1;
-        mainBox.position = new Vector3(5, ((3 / 2) * mainBox.scaling.y), 5);
-        mainBox.rotation.y = (Math.PI * 45) / 180;
-        mainBox.material = materialWall;
+        let columns: Array<Array<Mesh>> = [];
+        let nColumns = 6;
+        let sizeArena = 100 * boxArena.scaling.x - 50;
+        let ratio = ((100 / nColumns) / 100) * sizeArena;
+        for (let i = 0; i <= 1; i++) {
+            if (nColumns > 0) {
+                columns[i] = [];
+                let mainCylinder = Mesh.CreateCylinder(`cy10-${i}`, 30, 5, 5, 20, 4, scene);
+                mainCylinder.position = new Vector3(- sizeArena / 2, 30 / 2, - 20 + (40 * i));
+                mainCylinder.material = materialWall;
+                columns[i].push(mainCylinder);
 
-        let mainBox2 = mainBox.clone("box2");
-        mainBox2.scaling.y = 2;
-        mainBox2.position = new Vector3(5, ((3 / 2) * mainBox.scaling.y), -5);
-        mainBox2.material = materialWall;
-
-        let mainBox3 = mainBox.clone("box3");
-        mainBox3.scaling.y = 3;
-        mainBox3.position = new Vector3(-5, ((3 / 2) * mainBox.scaling.y), -5);
-        mainBox3.material = materialWall;
-
-        let mainBox4 = mainBox.clone("box4");
-        mainBox4.scaling.y = 4;
-        mainBox4.position = new Vector3(-5, ((3 / 2) * mainBox.scaling.y), 5);
-        mainBox4.material = materialWall;
-
-        let cylinder = Mesh.CreateCylinder("cyl1", 20, 5, 5, 20, 4, scene);
-        cylinder.position.y = 20 / 2;
-        cylinder.material = materialWall;
+                if (nColumns > 1) {
+                    for (let j = 1; j <= nColumns - 1; j++) {
+                        let newCyl = columns[i][0].clone(`cy1${j}-${i}`);
+                        newCyl.position = new Vector3(- (sizeArena / 2) + (ratio * j), 30 / 2, columns[i][0].position.z);
+                        columns[i].push(newCyl);
+                    }
+                }
+            }
+        }
     }
 }
