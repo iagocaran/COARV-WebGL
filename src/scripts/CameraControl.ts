6d8e7ec765b87450e1a595f138040636fbfd137e
Iago Caran Aquino <iago.caran@gmail.com>
2021-02-26 12:21:49 +0100

    Move camera position movement to Player class

--- src/scripts/CameraControl.ts
+++ src/scripts/CameraControl.ts
@@ -8,14 +8,10 @@ export class CameraControl implements ICameraInput<FreeCamera> {
     keysDown : Array<string> = ['ArrowDown', 's'];
     keys : Array<string> = [];
     noPreventDefault : boolean = false;
-    sensibility : number = -0.0015;
-    angularSensibility : number = -50;
-    speed : number = 0.5;
+    angularSensibility : number = 50;
     onKeyDown : any;
     onKeyUp : any;
     onMouseMove : any;
-    front : Vector3 = new Vector3(0, 1, 0);
-    up : Vector3 = new Vector3(0, 0, 1);
 
     constructor(camera : FreeCamera) {
         this.camera = camera;
@@ -54,7 +50,6 @@ export class CameraControl implements ICameraInput<FreeCamera> {
             }
         };
         this.onMouseMove = (event : MouseEvent) => {
-            // TODO Use Vector3.Lerp and move code to the checkInputs
             // TODO Tune sensitivity
             this.camera.cameraRotation.y += event.movementX * 0.001 * (this.angularSensibility / 250);
             let nextRotationX = this.camera.cameraRotation.x + (event.movementY * 0.001 * (this.angularSensibility / 250));
@@ -76,23 +71,5 @@ export class CameraControl implements ICameraInput<FreeCamera> {
         element?.removeEventListener("keyup", this.onKeyUp);
         element?.removeEventListener("mousemove", this.onMouseMove);
         this.keys = [];
-    }
-
-    checkInputs() {
-        for (let i = 0; i < this.keys.length; i++) {
-            let key = this.keys[i];
-            if (this.keysLeft.indexOf(key) !== -1) {
-                this.camera.cameraRotation.y += this.sensibility;
-            } else if (this.keysRight.indexOf(key) !== -1) {
-                this.camera.cameraRotation.y -= this.sensibility;
-            }
-            // TODO Use front position parallel to the world plane
-            if (this.keysUp.indexOf(key) !== -1) {
-                this.camera.position = Vector3.Lerp(this.camera.position, this.camera.position.subtract(this.camera.getFrontPosition(1).normalize().scale(this.speed)), 0.1);
-            } else if (this.keysDown.indexOf(key) !== -1) {
-                this.camera.position = Vector3.Lerp(this.camera.position, this.camera.position.add(this.camera.getFrontPosition(1).normalize().scale(this.speed)), 0.1);
-            }
-            this.camera._updatePosition();
-        }
     }
 }
--- src/scripts/Player.ts
+++ src/scripts/Player.ts
@@ -1,11 +1,13 @@
-import { Camera, FreeCamera, Scene, Vector3} from "@babylonjs/core";
+import {Angle, FreeCamera, Scene, TargetCamera, Vector3} from "@babylonjs/core";
 import { Game } from "./Game";
 import { CameraControl } from "./CameraControl";
 
 
 export class Player {
     scene : Scene;
-    camera : Camera;
+    camera : TargetCamera;
+    control? : CameraControl;
+    speed : number = 0.5;
     isAlive : boolean;
     controlEnabled : boolean = false;
     rotEngaged : boolean = false;
@@ -17,12 +19,13 @@ export class Player {
         this._initPointerLock();
     }
 
-    private _initCamera(scene : Scene, canvas : HTMLCanvasElement) : Camera {
+    private _initCamera(scene : Scene, canvas : HTMLCanvasElement) : TargetCamera {
         let camera = new FreeCamera("camera", new Vector3(-20, 5, 0), this.scene);
         camera.setTarget(Vector3.Zero());
         camera.inputs.removeByType("FreeCameraKeyboardMoveInput");
         camera.inputs.removeByType("FreeCameraMouseInput");
-        camera.inputs.add(new CameraControl(camera));
+        this.control = new CameraControl(camera);
+        camera.inputs.add(this.control);
         camera.attachControl(false);
         return camera;
     }
@@ -42,5 +45,43 @@ export class Player {
         };
 
         document.addEventListener("pointerlockchange", pointerLockChange, false);
+    }
+
+    checkMove(fpsRatio : number) {
+        let control = this.control;
+        let relativeSpeed = this.speed / fpsRatio;
+        if (control) {
+            for (let i = 0; i < control.keys.length; i++) {
+                let key = control.keys[i];
+                if (control.keysUp.indexOf(key) !== -1) {
+                    this.camera.position = new Vector3(
+                        this.camera.position.x + (Math.sin(this.camera.rotation.y) * relativeSpeed),
+                        this.camera.position.y,
+                        this.camera.position.z + (Math.cos(this.camera.rotation.y) * relativeSpeed)
+                    );
+                }
+                if (control.keysDown.indexOf(key) !== -1) {
+                    this.camera.position = new Vector3(
+                        this.camera.position.x + (Math.sin(this.camera.rotation.y) * -relativeSpeed),
+                        this.camera.position.y,
+                        this.camera.position.z + (Math.cos(this.camera.rotation.y) * -relativeSpeed)
+                    );
+                }
+                if (control.keysLeft.indexOf(key) !== -1) {
+                    this.camera.position = new Vector3(
+                        this.camera.position.x + Math.sin(this.camera.rotation.y + Angle.FromDegrees(-90).radians()) * relativeSpeed,
+                        this.camera.position.y,
+                        this.camera.position.z + Math.cos(this.camera.rotation.y + Angle.FromDegrees(-90).radians()) * relativeSpeed
+                    );
+                }
+                if (control.keysRight.indexOf(key) !== -1) {
+                    this.camera.position = new Vector3(
+                        this.camera.position.x + Math.sin(this.camera.rotation.y + Angle.FromDegrees(-90).radians()) * -relativeSpeed,
+                        this.camera.position.y,
+                        this.camera.position.z + Math.cos(this.camera.rotation.y + Angle.FromDegrees(-90).radians()) * -relativeSpeed
+                    );
+                }
+            }
+        }
     }
 }
